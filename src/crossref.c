7a5196e7a6e4d2d8457d75200fadd17df33dbede
Neil Horman <nhorman@tuxdriver.com>
2009-08-28 14:28:27 +0000

    The lexer, if passed a file with a c extension that has only a '=' or ':' symbol in it, will trigger lexer rules that expect my_yytext to be set.  Since no other rules have been triggered yet, my_yytext is NULL and a segfault occurs.  To fix this we add a LEXERR token and return it if my_yytext is null.  This causes the crossref generator to detect the lexer error and abort further processing of the file.

--- src/crossref.c
+++ src/crossref.c
@@ -45,7 +45,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-static char const rcsid[] = "$Id: crossref.c,v 1.13 2006/04/21 10:45:48 broeker Exp $";
+static char const rcsid[] = "$Id: crossref.c,v 1.14 2006/07/23 20:59:20 broeker Exp $";
 
 
 /* convert long to a string */
@@ -176,6 +176,7 @@ crossref(char *srcfile)
 #endif
 	    break;
 			
+	case LEXERR:	/* Lexer error, abort further parsing of this file */
 	case LEXEOF:	/* end of file; last line may not have \n */
 			
 			/* if there were symbols, output them and the source line */
