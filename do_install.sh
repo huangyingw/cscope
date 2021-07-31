446fe5f59da9d78bddd0928a5d63235f6e784ff5
Ying Huang <huangyingw@gmail.com>
2021-07-30 22:34:09 -0700

    n

---
+++ do_install.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+if [ $(uname) != "Darwin" ] ; \
+then \
+    apt-get update ; \
+    apt-get install -y \
+    automake \
+    autotools-dev \
+    bison \
+    byacc \
+    flex \
+    libncurses5-dev \
+    libncursesw5-dev
+else \
+    brew install \
+    autoconf \
+    automake \
+    bison \
+    byacc \
+    flex \
+    libtool
+fi
+
+aclocal && \
+    autoconf && \
+    automake && \
+    ./configure && \
+    make clean && \
+    make && \
+    sudo make install
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
