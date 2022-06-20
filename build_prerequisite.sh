2327039767893342a589f6ef9a3f43a018401459
Ying Huang <huangyingw@gmail.com>
2022-06-20 16:18:03 -0700

    n

--- build_prerequisite.sh
+++ build_prerequisite.sh
@@ -3,26 +3,30 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ $(uname) != "Darwin" ]
-then
-    apt-get update
+apt-get update ; \
     apt-get install -y \
-        automake \
-        autotools-dev \
-        bison \
-        byacc \
-        flex \
-        libncurses5-dev \
-        libncursesw5-dev
+    automake \
+    autotools-dev \
+    bison \
+    byacc \
+    flex \
+    libncurses5-dev \
+    libncursesw5-dev
 
-else
-    brew install \
-        autoconf \
-        automake \
-        bison \
-        byacc \
-        flex \
-        libtool
-fi
+brew install \
+    autoconf \
+    automake \
+    bison \
+    byacc \
+    flex \
+    libtool
 
-make clean
+pacman -Syy && \
+    pacman --noconfirm -Sy \
+    autoconf \
+    automake \
+    bison \
+    byacc \
+    flex \
+    libtool \
+    make
